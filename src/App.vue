ac1f276eb9c3c16bee68db6ea8237c5a98261578
PaoloCentoscudi <paolocentoscudi@gmail.com>
2023-08-25 00:27:31 +0200

    add projectcard

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,25 @@
 import axios from 'axios';
 import { createApp } from 'vue';
 
+import ProjectCard from './components/ProjectCard.vue';
+
+
 const API = "http://127.0.0.1:8000/api/z0"
 
 export default{
   name:createApp,
+  components:{
+    ProjectCard
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
   mounted(){
     axios.get(API + '/projects')
       .then(res=>{
-        console.log(res.data);
+        this.projects=res.data.projects;
       })
       .catch(err => console.error(err));
   }
@@ -19,8 +30,16 @@ export default{
 </script>
 
 <template>
-  <h1>Aooo</h1>
+  <h1>Projects</h1>
+  <div class="row">
+    <ProjectCard v-for="project in projects" :key="project.id" :project="project"/>
+  </div>
 </template>
 
 <style scoped>
+.row{
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
 </style>
---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+    props:{
+        project:{
+            type: Object
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <h2>{{ project.name }}</h2>
+        <p>{{ project.description }}</p>
+        <span>{{ project.difficulty }}</span>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    width: 30%;
+    border: 1px solid black;
+    margin-bottom: 30px;
+}   
+</style>
